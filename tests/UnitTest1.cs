53411608309520583a222e17bc6b7d4c56ee22fc
Chris Hamons <chris.hamons@gmail.com>
2019-04-07 21:11:17 -0500

    C# Project framing

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace IncrementalSociety.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

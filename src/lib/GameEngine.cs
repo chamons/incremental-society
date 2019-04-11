9ebf579fb917ee84117c6e301e20a3ba4855ccf3
Chris Hamons <chris.hamons@gmail.com>
2019-04-10 23:28:47 -0500

    Flesh out region section

---
+++ src/lib/GameEngine.cs
@@ -0,0 +1,15 @@
+﻿using IncrementalSociety.Model;
+
+namespace IncrementalSociety
+{
+    public class GameEngine
+    {
+        public static GameState CreateNewGame ()
+        {
+            var greenlandRegion = new Region ("Greenland", new Area[] { new Area (AreaType.Forest), new Area (AreaType.Plains), new Area (AreaType.Forest), new Area (AreaType.Forest) , new Area (AreaType.Ocean) });
+            var mudFlatsRegion = new Region ("Mudflats", new Area[] { new Area (AreaType.Swamp), new Area (AreaType.Swamp), new Area (AreaType.Swamp), new Area (AreaType.Plains), new Area (AreaType.Desert) });
+            var resources = new Resource[] { new Resource ("Food", 100), new Resource ("Wood", 50) };
+            return new GameState (Age.Stone, new Region[] { greenlandRegion, mudFlatsRegion }, resources);
+        }
+    }
+}

3c09f7cbb9efa4fb05ec3efac7521832701fffc4
Chris Hamons <chris.hamons@xamarin.com>
2019-05-05 20:44:54 -0400

    Add plumbing to select builds for destory/add

--- src/IncrementalSociety.Web/Services/GameService.cs
+++ src/IncrementalSociety.Web/Services/GameService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,13 +8,21 @@ using System.Collections.Immutable;
 
 namespace IncrementalSociety.Web.Services
 {
+	public enum GameUIState { Default, SelectForBuild, SelectForDestory }
+
 	public class GameService
 	{
 		JsonLoader Loader;
-
 		GameEngine Engine;
+		public Action NotifyUIStateHasChanged;
+
+		public GameUIState CurrentUIState { get; private set; } = GameUIState.Default;
 		public GameState State { get; private set; }
 		public int RegionCapacity { get; private set; }
+	
+		const string CancelText = "Cancel";
+		const string BuildText = "Build District";
+		const string DestroyText = "Destory District";
 
 		public GameService ()
 		{
@@ -21,13 +30,23 @@ namespace IncrementalSociety.Web.Services
 			RegionCapacity = Loader.Game.RegionCapacity;
 			State = GameEngine.CreateNewGame ();
 			Engine = new GameEngine ();
+			
+			ResetActionList ();
+		}
+
+		void ResetActionList ()
+		{
+			Actions = new List<string> (Loader.Actions.Actions.Select (x => x.Name));
+			Actions.Add (BuildText);
+			Actions.Add (DestroyText);
 		}
 
 		// STUB_DATA - Filter by age
 		public IEnumerable<ResourceDeclaration> Resources => Loader.Resources.Resources;
-		public IEnumerable<GameAction> Actions => Loader.Actions.Actions;
 		public IEnumerable<Region> Regions => State.Regions;
 		public ImmutableDictionary<string, double> GetNextTickResources () => Engine.GetResourcesNextTick (State);
+		
+		public List<string> Actions { get; private set; }
 
 		public string GetImageFilename (string name)
 		{
@@ -49,8 +68,67 @@ namespace IncrementalSociety.Web.Services
 		}
 
 		public void ApplyAction (string action)
-		{			
-			State = Engine.ApplyAction (State, action);
+		{
+			NotifyUIStateHasChanged ();
+			switch (action)
+			{
+				case BuildText:
+					SetUIState (GameUIState.SelectForBuild);
+					return;
+				case DestroyText:
+					SetUIState (GameUIState.SelectForDestory);
+					return;
+				case CancelText:
+					SetUIState (GameUIState.Default);
+					return;
+				default:
+					SetUIState (GameUIState.Default);
+					State = Engine.ApplyAction (State, action);
+					return;
+			}
+		}
+
+		void SetUIState (GameUIState state)
+		{
+			ResetActionList ();
+			CurrentUIState = state;
+			if (state != GameUIState.Default)
+				ReplaceActionWithCancel (state);
+		}
+
+		void ReplaceActionWithCancel (GameUIState state)
+		{
+			string actionText = GetActionTextForState (state);
+			for (int i = 0 ; i < Actions.Count ; ++i) {
+				if (Actions[i] == actionText) {
+					Actions[i] = CancelText;
+					return;
+				}
+			}
+		}
+
+		string GetActionTextForState (GameUIState state)
+		{
+			switch (state)
+			{
+				case GameUIState.SelectForBuild:
+					return BuildText;
+				case GameUIState.SelectForDestory:
+					return DestroyText;
+			}
+			throw new InvalidOperationException ($"GetActionTextForState with state {state}");
+		}
+
+		public void OnBuildSelection (Region r, int position)
+		{
+			SetUIState (GameUIState.Default);
+			NotifyUIStateHasChanged ();
+		}
+		
+		public void OnDestroySelection (Region r, int position)
+		{
+			SetUIState (GameUIState.Default);
+			NotifyUIStateHasChanged ();
 		}
 
 		public void OnTick ()

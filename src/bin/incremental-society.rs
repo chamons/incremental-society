5211ce8741465f809ddbee277ee4ced51bab1d7e
Chris Hamons <chris.hamons@gmail.com>
2020-04-11 14:55:20 -0500

    Console refactor WIP

--- src/bin/incremental-society.rs
+++ src/bin/incremental-society.rs
@@ -1,16 +1,13 @@
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use incremental_society::console_ui::{self, OptionList, Selection};
+use incremental_society::console_ui::{OptionList, Selection};
 use incremental_society::engine;
 use incremental_society::state::{format_resource_list, Building, DelayedAction, GameState, ResourceKind, Upgrade, MAX_UPGRADES, NUM_RESOURCES};
 
 use pancurses::{Input, Window};
 
 fn main() {
-    let term = pancurses::initscr();
-    console_ui::init_colors();
-
     let mut ui = UI {
         messages: "".to_string(),
         message_timeout: 0,
@@ -24,6 +21,12 @@ struct UI<'a> {
     message_timeout: u32,
     term: &'a Window,
 }
+
+// Need current job position
+// Draw one for each job
+// Draw current highlightned some how
+// Handle + to add-job
+// Handle - to sub-job
 
 impl<'a> UI<'a> {
     fn main(&mut self) {
@@ -233,158 +236,6 @@ impl<'a> UI<'a> {
         }
 
         false
-    }
-
-    #[allow(unused_assignments)]
-    fn draw(&self, state: &GameState) {
-        self.term.clear();
-
-        let mut y = 1;
-
-        // Left Column
-        y = self.draw_country_stats(state, y);
-        y += 1;
-        y = self.draw_resources(state, y);
-
-        // Right Column
-        y = 1;
-        y = self.draw_regions(state, y);
-        y += 1;
-        y = self.draw_conversions(state, y);
-
-        self.draw_messages();
-        self.draw_prompt();
-    }
-
-    fn draw_conversions(&self, state: &GameState, y: i32) -> i32 {
-        let mut y = y;
-        const CONVERSION_BAR_LENGTH: f64 = 30.0;
-
-        y = self.write_right("Conversions", 0, y);
-        y += 1;
-
-        for c in &state.actions {
-            let percentage = c.percentage();
-
-            // Don't update y, as we have to draw the bar
-            if let DelayedAction::Conversion(name) = &c.action {
-                let count = state.derived_state.current_building_jobs.get(name).unwrap();
-                self.write_right(&format!("{} ({})", name, count), 0, y);
-            } else {
-                self.write_right(&c.name, 0, y);
-            }
-
-            let filled_width = (CONVERSION_BAR_LENGTH * percentage).round();
-            let empty_width = (CONVERSION_BAR_LENGTH - filled_width).round() as usize;
-            let filled_width = filled_width as usize;
-            let bar_text = format!("{}{}", "#".repeat(filled_width), "-".repeat(empty_width));
-            y = self.write_right(&bar_text, c.name.len() as i32 + 5, y);
-        }
-        y
-    }
-
-    fn write_region_contents(&self, text: &str, x: i32, y: i32) -> i32 {
-        // RIGHT_COL_WIDTH - 2
-        self.write_right(&format!("|{: <67}|", text), x, y)
-    }
-
-    #[allow(unused_assignments)]
-    fn draw_regions(&self, state: &GameState, y: i32) -> i32 {
-        if !self.should_draw_buildings(state) {
-            return 0;
-        }
-
-        let mut y = y;
-        for r in &state.regions {
-            y = self.write_right("---------------------------------------------------------------------", 0, y);
-
-            y = self.write_region_contents(&r.name, 0, y);
-
-            let mut x = 0;
-            let building_top_line = y;
-            for b in 0..r.max_building_count() {
-                if let Some(building) = r.buildings.get(b) {
-                    y = building_top_line;
-
-                    let building_name = &building.name;
-                    let building_name_length: usize = building_name.len();
-
-                    // Draw box manually
-                    self.write("|", UI::RIGHT_COL, y);
-                    self.write("|", UI::RIGHT_COL, y + 1);
-                    self.write("|", UI::RIGHT_COL, y + 2);
-                    self.write("|", UI::RIGHT_COL + UI::RIGHT_COL_WIDTH - 1, y);
-                    self.write("|", UI::RIGHT_COL + UI::RIGHT_COL_WIDTH - 1, y + 1);
-                    self.write("|", UI::RIGHT_COL + UI::RIGHT_COL_WIDTH - 1, y + 2);
-
-                    y = self.write_right(&"_".repeat(building_name_length + 2), x + 2, y);
-                    y = self.write_right(&format!("|{}|", building_name), x + 2, y);
-                    y = self.write_right(&"-".repeat(building_name_length + 2), x + 2, y);
-
-                    x += building_name_length as i32 + 3;
-                }
-            }
-            y = self.write_right("---------------------------------------------------------------------", 0, y);
-        }
-
-        y
-    }
-
-    fn should_draw_buildings(&self, state: &GameState) -> bool {
-        state.age != "Archaic"
-    }
-
-    #[allow(unused_assignments)]
-    fn draw_country_stats(&self, state: &GameState, y: i32) -> i32 {
-        let mut y = self.write("Elysium", 1, y);
-        y += 1;
-        y = self.write(format!("{} Age", state.age), 1, y);
-        if self.should_draw_buildings(state) {
-            y = self.write(format!("Population: {}", state.pops), 1, y + 1);
-        }
-        y = self.write(" ----------------", 0, y + 1);
-
-        y
-    }
-
-    fn draw_resources(&self, state: &GameState, y: i32) -> i32 {
-        let mut y = y;
-
-        for i in 0..NUM_RESOURCES {
-            let line = &format!(
-                "{}: {} / {}",
-                ResourceKind::name_for_index(i),
-                state.resources[i],
-                state.derived_state.storage[i]
-            );
-            y = self.write(line, 1, y);
-        }
-        y
-    }
-
-    fn draw_messages(&self) {
-        self.write(&self.messages, 2, self.term.get_max_y() - 2);
-    }
-
-    fn draw_prompt(&self) {
-        let x = self.term.get_max_x();
-        let y = self.term.get_max_y();
-        self.write(" ", x - 1, y - 1);
-    }
-
-    fn write<S>(&self, text: S, x: i32, y: i32) -> i32
-    where
-        S: Into<String>,
-    {
-        self.term.mvaddstr(y, x, text.into());
-        y + 1
-    }
-
-    const RIGHT_COL: i32 = 50;
-    const RIGHT_COL_WIDTH: i32 = 69;
-
-    fn write_right(&self, text: &str, x: i32, y: i32) -> i32 {
-        self.write(text, x + UI::RIGHT_COL, y)
     }
 }
 
--- src/console_ui/mod.rs
+++ src/console_ui/mod.rs
@@ -1,5 +1,7 @@
 mod colors;
 mod option_list;
+mod screen;
 
 pub use colors::{clear_color, init_colors, set_color, Colors};
 pub use option_list::{OptionList, Selection};
+pub use screen::Screen;
